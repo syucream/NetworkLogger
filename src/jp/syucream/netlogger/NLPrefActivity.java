2c08bfe6fd28f1477bd5361e5de7c388fb03ff5d
syu_cream <ryo@ryo-MBP-2.local>
2012-02-25 23:51:48 +0900

    * Notification 機能の追加

--- src/jp/syucream/netlogger/NLPrefActivity.java
+++ src/jp/syucream/netlogger/NLPrefActivity.java
@@ -1,19 +1,14 @@
 package jp.syucream.netlogger;
 
+import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
-import android.provider.Settings;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.widget.ProgressBar;
+
 
 /**
  * 設定画面を表示する
@@ -21,9 +16,31 @@ import android.widget.ProgressBar;
  */
 public class NLPrefActivity extends PreferenceActivity {
 
+    // Notification 関連
+    private final static String TITLE = "Network Logger";
+    private final static String MSG = "Start logging...";
+
     @Override
     protected void onCreate( Bundle savedInstanceState ){
         super.onCreate( savedInstanceState );
         addPreferencesFromResource( R.xml.pref );
+
+        // notification
+        if( PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getBoolean("notification", true) )
+            setNotification(getBaseContext());
+    }
+
+    /**
+     * Notification を用いて動作状況をユーザに通知します
+     */
+    public static void setNotification(Context context){
+        NotificationManager nm = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification n = new Notification(R.drawable.icon, TITLE + " : " + MSG, System.currentTimeMillis());
+        PendingIntent intent = PendingIntent.getActivity(context, 0,
+                new Intent(context, NLPrefActivity.class), Intent.FLAG_ACTIVITY_NEW_TASK);
+        n.setLatestEventInfo(context, TITLE, MSG, intent);
+        n.flags = Notification.FLAG_ONLY_ALERT_ONCE;
+        nm.cancel(0);
+        nm.notify(0, n);
     }
 }
